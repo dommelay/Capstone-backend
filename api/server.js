dae6ed0f7bc7f69eee3b90e7a1d49280a6641daf
Dominique Clark <melaydominique@gmail.com>
2023-05-01 17:25:16 -0400

    env/deployment

--- api/server.js
+++ api/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const db = mongoose.connection;
+require('dotenv').config()
 const cors = require('cors')
-app.use(cors())
-app.use(express.json())
 const Artworks = require('./models/artwork.js')
 const MyArtworks = require('./models/myartwork.js')
+
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI)
+
+app.use(express.static('public'));
+app.use(cors())
+app.use(express.json())
 
 app.get('/artworks', (req, res) => {
     Artworks.find({}).then((foundArtworks) => {
@@ -41,9 +49,10 @@ mongoose.connection.once('open', () => {
     console.log('connected to mongod...')
 })
 
-app.listen(3000, () => {
-    console.log('listening...')
-})
+app.listen(PORT, () => console.log( 'Listening on port:', PORT));
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongod...');
+});
 
 
 
